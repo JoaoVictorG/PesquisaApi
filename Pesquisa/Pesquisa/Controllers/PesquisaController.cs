ab7baec1690dd56559b036def5749739e2422639
JoaoVictorG <joaovictorgon36@gmail.com>
2022-12-09 09:17:54 -0300

    comentario do download do html

--- Pesquisa/Pesquisa/Controllers/PesquisaController.cs
+++ Pesquisa/Pesquisa/Controllers/PesquisaController.cs
@@ -35,6 +35,18 @@ namespace PesquisaApi.Controllers
             }
         }
 
+        //[HttpGet("{nome}")]
+        //public string GetPesquisa(string nome)
+        //{
+        //    string link = $"https://www.google.com/search?q={nome}";
+        //    using (var client = new WebClient())
+        //    {
+        //        var response = client.DownloadString(link);
+        //        return response;
+        //    }
+
+        //}
+
     }
 }
 
